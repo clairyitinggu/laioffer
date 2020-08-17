fd34e7ab6655215ce12da41d2419f87a5aa3097a
clairyitinggu <yitinggu@umass.edu>
2020-08-17 15:23:03 -0400

    updated

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import "./assets/css/style.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+registerServiceWorker();
