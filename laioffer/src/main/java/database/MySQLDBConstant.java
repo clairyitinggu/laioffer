a070c8daa6777e9cc0e1a0cc0e1ad2cc429f820f
kira0415 <kirahuang0415@gmail.com>
2020-08-23 16:23:10 -0400

    Change to new database instance

--- laioffer/src/main/java/database/MySQLDBConstant.java
+++ laioffer/src/main/java/database/MySQLDBConstant.java
@@ -1,11 +1,11 @@
 package database;
 
 public class MySQLDBConstant {
-	private static final String INSTANCE = "laiproject-instance.cbqxhypf3tie.us-east-2.rds.amazonaws.com";
+	private static final String INSTANCE = "laiproject-instance.coz9llegvrth.us-east-2.rds.amazonaws.com";
 	private static final String PORT_NUM = "3306";
 	public static final String DB_NAME = "laiproject";
 	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "kira201314";
+	private static final String PASSWORD = "123456789";
 	public static final String URL = "jdbc:mysql://"
 			+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
