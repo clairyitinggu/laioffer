d847a5e3e0c3b2ceef99b30e5013a46a6c03c6db
Lusha Zhang <lzhang9011@gmail.com>
2020-08-28 22:25:59 -0400

    wrapped components, added Dashboard, link to Form

--- src/index.js
+++ src/index.js
@@ -7,15 +7,16 @@ import "./assets/css/style.css";
 import Form from "./components/Form";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-<<<<<<< HEAD
-ReactDOM.render(<Form />, document.getElementById("root"));
-=======
+
+// ReactDOM.render(<Form />, document.getElementById("root"));
+
 ReactDOM.render(
     <BrowserRouter>
         <App />
     </BrowserRouter>,
     document.getElementById("root"));
->>>>>>> 840b046b9336df31c3cbfba308db6233ccf9524f
+
 
 registerServiceWorker();
